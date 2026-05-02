4f73fb7bc06f42521b93ae2ae824c89a2d0f2d5c
rewna20 <anwer.benabdallah01@gmail.com>
2026-05-02 17:17:04 +0100

    updated model file

--- src/main/java/gl2/miniproject/todolist/model/Task.java
+++ src/main/java/gl2/miniproject/todolist/model/Task.java
@@ -1,6 +1,9 @@
 package gl2.miniproject.todolist.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.time.LocalDateTime;
 
@@ -9,17 +12,62 @@ import java.time.LocalDateTime;
 
 @Entity
 public class Task {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	
+	
 	private String name;
+	
 	private boolean finished;
+	
 	private LocalDateTime creationDate;
-//test
+	
 	
 	// constructeurs:
 	
+	public Task() {
+		this.creationDate =LocalDateTime.now(); 
+	}
+	
+	public Task(String name) {
+		this.name=name;
+		this.finished=false; // par defaut non fait
+		this.creationDate=LocalDateTime.now();
+	}
 
-
+	// getters:
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public boolean isFinished() {
+		return finished;
+	}
+	public LocalDateTime getCreationDate() {
+		return creationDate;
+	}
+	
+	// setters:
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+    
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
 
 
 	 
