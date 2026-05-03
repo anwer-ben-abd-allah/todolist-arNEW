c7831b4016a93a0998842fc78abc87d117c4597a
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-05-03 23:31:48 +0200

    feat : add modifyName and modifyStatus endpoints

--- src/main/java/gl2/miniproject/todolist/service/TaskService.java
+++ src/main/java/gl2/miniproject/todolist/service/TaskService.java
@@ -3,15 +3,12 @@ package gl2.miniproject.todolist.service;
 import java.util.List;
 import java.util.Optional;
 
-import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import gl2.miniproject.todolist.model.Task;
-import gl2.miniproject.todolist.model.User;
 import gl2.miniproject.todolist.repository.TaskRepository;
-import gl2.miniproject.todolist.repository.UserRepository;
 
 @Service
 public class TaskService {
@@ -39,7 +36,23 @@ public class TaskService {
 	public Task modify(Task task) {
 		return taskRepository.save(task);
 	}
+	
 	public Optional<Task> getTaskById(Long id) {
 		return taskRepository.findById(id);
+	}
+		
+		
+	public Task modifyName(Long id, String newName) {
+	    Task task = taskRepository.findById(id)
+	        .orElseThrow(() -> new RuntimeException("Task not found"));
+	    task.setName(newName);
+	    return taskRepository.save(task);
+	}
+
+	public Task modifyStatus(Long id) {
+	    Task task = taskRepository.findById(id)
+	        .orElseThrow(() -> new RuntimeException("Task not found"));
+	    task.setFinished(!task.isFinished()); 
+	    return taskRepository.save(task);
 	}
 }
